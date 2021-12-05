148b9fb85e2704ac7328f3f3078bef8e9b78942d
Harold Mendoza <slu.mendozaharold@gmail.com>
2021-12-05 22:01:07 +0800

    add Glass.is_full test

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -1,6 +1,20 @@
-from water_overflow_solution.app.main import greet
+import pytest
+
+from water_overflow_solution.app.main import greet, Glass
 
 
 def test_greet():
     name = 'James'
     assert greet(name) == 'Hi, James!'
+
+
+@pytest.mark.parametrize(
+    "glass_input,expected",
+    [
+        (Glass(capacity=10, fill_level=10), True),
+        (Glass(capacity=15, fill_level=10), False),
+        (Glass(capacity=300, fill_level=400), True)
+    ]
+)
+def test_glass_is_full(glass_input, expected):
+    assert glass_input.is_full() == expected
