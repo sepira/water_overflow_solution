8e19e51684e7c74a81f2c54345ac307f2a2c8357
Harold Mendoza <slu.mendozaharold@gmail.com>
2021-12-05 22:55:36 +0800

    add calculate_water_level logic

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Glass:
     def __init__(self, capacity: int, fill_level: int = 0) -> None:
         self.capacity = capacity
@@ -16,6 +19,27 @@ class Glass:
         if self.fill_level >= self.capacity:
             return True
         return False
+
+
+def calculate_water_level(poured) -> List[List[Glass]]:
+    capacity = 250
+    water_left = int(poured * 1000)  # L to mL conversion
+    max_row = water_left // capacity
+    glass_tower = [[Glass(capacity=capacity) for _ in range(j + 1)] for j in range(max_row)]
+    glass_tower[0][0].fill_level = water_left
+    row = 0
+
+    while row < max_row and water_left > capacity:
+        for tmp_glass_pos in range(row):
+            tmp_row = row - 1
+            if glass_tower[tmp_row][tmp_glass_pos].is_full():
+                fill_remainder = glass_tower[tmp_row][tmp_glass_pos].fill_remainder()
+                glass_tower[tmp_row][tmp_glass_pos].fill_level = capacity
+                glass_tower[tmp_row + 1][tmp_glass_pos].fill_level += fill_remainder / 2.0
+                glass_tower[tmp_row + 1][tmp_glass_pos + 1].fill_level += fill_remainder / 2.0
+                water_left -= capacity
+        row += 1
+    return glass_tower
 
 
 def greet(name):
